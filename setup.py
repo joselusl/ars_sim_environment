2e061997744454a94801b96492d745673e7e46c7
Jose Luis SANCHEZ-LOPEZ <joselusl@hotmail.com>
2026-08-27 17:25:34 +0200

    minor updates for compatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
  maintainer_email='joseluis.sanlop@gmail.com',
  description='TODO: Package description',
  license='BSD',
- tests_require=['pytest'],
  entry_points={'console_scripts': [
  		'ars_sim_environment_ros_node = ars_sim_environment.ars_sim_environment_ros_node:main',
         ],},
